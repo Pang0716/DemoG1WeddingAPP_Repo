b10589b006cd060e71f273dafe756912f39d0690
Sean <pangchunsean@gmail.com>
2026-08-10 16:38:47 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/dcsg1weddingapp_githubdemo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.dcsg1weddingapp_githubdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dcsg1weddingapp_githubdemo.ui.theme.DCSG1WeddingAPP_GithubDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DCSG1WeddingAPP_GithubDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DCSG1WeddingAPP_GithubDemoTheme {
+        Greeting("Android")
+    }
+}
